6f16c8254f3a17e4551afc0966ccf88e9b74540f
777marc@gmail.com <777marc@gmail.com>
2020-10-20 22:01:43 -0400

    tween

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -11,26 +11,38 @@ class SceneMain extends Phaser.Scene {
     
     create() {
         this.char = this.add.sprite(0, game.config.height/2, 'boy');
-
         var frameNames= this.anims.generateFrameNumbers('boy');
-
         this.anims.create({
             key: 'walk',
             frames: frameNames,
             frameRate: 10,
             repeat: -1
         });
-
         this.char.play('walk');
+        this.doWalk();
+    }
 
-        this.tweens.add({targets: this.char, duration: 1000, x:game.config.width});
+    doWalk() {
+        this.tweens.add({
+            targets: this.char,
+            duration: 5000,
+            x:game.config.width,
+            y:0,
+            alpha:0,
+            onComplete:this.onCompleteHandler.bind(this)
+        });
+    }
+
+    onCompleteHandler(tween, targets) {
+        console.log('complete');
+        let char = targets[0];
+        char.x = 20;
+        char.y = game.config.height / 2;
+        char.alpha = 1;
+        this.doWalk();
     }
 
     update() {
-        this.char.x++;
 
-        if(this.char.x > game.config.width) {
-            this.char.x = 0;
-        }
     }
 }
